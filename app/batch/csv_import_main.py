db6c46edee422d65fa2747447255ee0dde426c36
fujimaru-lab <yoshihiro.fujita@i-vinci.co.jp>
2020-06-11 22:10:24 +0900

    [modify] 原材料情報取り込み処理と保存方法情報取り込み処理を追加

--- app/batch/csv_import_main.py
+++ app/batch/csv_import_main.py
@@ -5,35 +5,47 @@ import psycopg2
 import sys
 sys.path.append('batch/dao')
 import dao.material_dao as m_dao
+import dao.prsv_method_dao as pm_dao
 import os
 
 def import_csv(file_path):
     script_nm = os.path.basename(sys.argv[0])
-    with open(file_path, newline='', encoding='utf-8_sig') as csvfile:
-        csvreader = csv.reader(csvfile)
-        
-        # CSVファイルの命名規則 ###############################
-        # <テーブル名>-data.csv
-        # ####################################################
-        filename = re.split('/', re.split('-', file_path)[0])
-        tbl_nm = filename[len(filename) - 1]
-        for record in csvfile:
-            if tbl_nm == "material":
-                m_dao.reg_material_data(re.split(',', record), script_nm)
-            elif tbl_nm == "material_set":
-                pass
-            elif tbl_nm == "prsv_method":
-                pass
-            elif tbl_nm == "maker_comp":
-                pass
-            elif tbl_nm == "prod_base":
-                pass
-            elif tbl_nm == "prod_sale":
-                pass
-            else:
-                print("no match")
-
-    csvfile.close()
+    try:
+        with open(file_path, newline='', encoding='utf-8_sig') as csvfile:
+            csvreader = csv.reader(csvfile)
+            
+            # CSVファイルの命名規則 ###############################
+            # <テーブル名>-data.csv
+            # ####################################################
+            filename = re.split('/', re.split('-', file_path)[0])
+            tbl_nm = filename[len(filename) - 1]
+            for record in csvfile:
+                if tbl_nm == "material":
+                    # 原材料情報、原材料セット情報取り込み
+                    m_dao.reg_material_data(re.split(',', record), script_nm)
+                elif tbl_nm == "prsv_method":
+                    # 保存方法情報取り込み
+                    pm_dao.reg_prsv_method_data(re.split(',', record), script_nm)
+                elif tbl_nm == "maker_comp":
+                    # 製造会社情報取り込み
+                    pass
+                elif tbl_nm == "maker_base":
+                    # 製造会社製造拠点情報取り込み
+                    pass
+                elif tbl_nm == "prod_base":
+                    # 商品基本情報取り込み
+                    pass
+                elif tbl_nm == "prod_sale":
+                    # 商品販売情報取り込み
+                    pass
+                else:
+                    print(filename, "is not subject to csv-import")
+                    break
+            
+    except Exception as e:
+        print(e)
+    finally:
+        csvfile.close()
 
 if __name__ == "__main__":
     filepath = sys.argv[1]
