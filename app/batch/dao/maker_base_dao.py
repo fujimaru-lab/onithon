2e13315e4d06484baeedfed3602512081e246bb4
fujimaru-lab <yoshihiro.fujita@i-vinci.co.jp>
2020-06-13 21:17:50 +0900

    [new]　製造会社製造拠点取り込み処理を新規作成

---
+++ app/batch/dao/maker_base_dao.py
@@ -0,0 +1,34 @@
+# encoding utf-8
+from dao import psgr_db_conn
+
+def reg_maker_base_data(record, scrpt_nm):
+    maker_comp_id = record[0]
+    maker_base_id = record[1]
+    base_name = record[2]
+    base_adrs = record[3]
+    base_tel = record[4]
+
+    try:
+        conn = psgr_db_conn.Connection()
+        cursor = conn.cursor()
+
+        upsrt = """
+            insert into maker_base
+            values (%s, %s, %s, %s, %s, %s, now(), %s, now())
+            on conflict (maker_comp_id, maker_base_id)
+            update set
+                base_name = %s,
+                base_adrs = %s,
+                base_tel = %s,
+                upd_user = %s,
+                upd_date = now()
+            """
+        sql_param = (maker_comp_id, maker_base_id, base_name, base_adrs, base_tel, scrpt_nm, scrpt_nm, base_name, base_adrs, base_tel, scrpt_nm)
+        cursor.execute(upsrt, sql_param)
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise e
+    finally:
+        cursor.close()
+        conn.close()
