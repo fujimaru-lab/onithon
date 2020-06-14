55f694bd2ad33a4b58317cda329277689eba696d
fujimaru-lab <yoshihiro.fujita@i-vinci.co.jp>
2020-06-14 23:15:48 +0900

    [modify] 設定ファイルから接続情報を取得するように変更

--- app/batch/dao/psgr_db_conn.py
+++ app/batch/dao/psgr_db_conn.py
@@ -1,18 +1,22 @@
 # encoding utf-8
 import psycopg2
+import conf.onithon_config as conf
 
-# TODO:シングルトン実装
+
+# TODO:シングルトン実装 
+# loggingクラスがドキュメント読む限りシングルトン実装？ https://github.com/python/cpython/blob/3.8/Lib/logging/__init__.py
 class Connection:
     """
     コネクション情報を保持する
     インスタンスメソッドはPEP249準拠
     """
     def __init__(self):
-        db = "mydb"
-        usr = "training"
-        pw = "training"
-        hst = "localhost"
-        prt = "5432"
+        config = conf.OnithonConfig()
+        db = config.getvalue('DB_CONN_INFO', 'ONITHON_DB')
+        usr = config.getvalue('DB_CONN_INFO', 'ONITHON_DB_USER')
+        pw = config.getvalue('DB_CONN_INFO', 'ONITHON_DB_PW')
+        hst = config.getvalue('DB_CONN_INFO', 'ONITHON_DB_HST')
+        prt = config.getvalue('DB_CONN_INFO', 'ONITHON_DB_PORT')
         self.conn = psycopg2.connect(database = db, user = usr, password = pw, host = hst, port = prt)
     
     def close(self):
