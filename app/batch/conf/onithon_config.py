5cc5964265c06e4ba007b913cc7e9c22d6787413
fujimaru-lab <yoshihiro.fujita@i-vinci.co.jp>
2020-06-14 23:13:21 +0900

    [bugfix] 設定ファイルの指定を修正

--- app/batch/conf/onithon_config.py
+++ app/batch/conf/onithon_config.py
@@ -1,12 +1,20 @@
 # encoding utf-8
-import  configparser
+import configparser
+import os
+
 
 class OnithonConfig:
+    '''
+    Onithonバッチ用設定ファイル読み込みクラス
+    onithon_config.iniより設定値を読み込み返す
+    '''
     def __init__(self):
+        dir_path = os.path.dirname(__file__)
+        self._conf_file = dir_path + '/onithon_config.ini'
         self._config = configparser.ConfigParser()
-        self._config.read('onithon_config.ini')
+        self._config.read(self._conf_file)
 
     def getvalue(self, key1, key2):
+        self._config.read(self._conf_file)
         value = self._config[key1][key2]
         return value
-    
